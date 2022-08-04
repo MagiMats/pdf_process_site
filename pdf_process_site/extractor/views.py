3aaccc4fc71e9740c7e2cbd0932399a831309856
MagiMats <matsjanssens2004@gmail.com>
2022-08-04 17:50:09 +0200

    first test form

--- pdf_process_site/extractor/views.py
+++ pdf_process_site/extractor/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import render
-
+from .forms import UploadForm
 
 # Create your views here.
 def HomeView(request):
-    return render(request, 'extractor\home.html', {})
+    if request.method == "POST":
+        form = UploadForm(request.POST)
+        if form.is_valid():
+            name = form.cleaned_data['name']
+            email = form.cleaned_data['email']
+
+            print(name, email)
+
+    return render(request, 'extractor\home.html', {'test':UploadForm})
