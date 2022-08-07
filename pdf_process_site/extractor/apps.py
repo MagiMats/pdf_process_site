b5d7386b268189f3291597920414ff1d85b9fba5
MagiMats <matsjanssens2004@gmail.com>
2022-08-07 09:43:15 +0200

    basic file upload working

--- pdf_process_site/extractor/apps.py
+++ pdf_process_site/extractor/apps.py
@@ -1,12 +1,13 @@
 from django.apps import AppConfig
-
+from pdf_process_site.settings import MEDIA_DIR
+import os
 
 class ExtractorConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'extractor'
 
 def handle_upload(file):
-    with open('C:\\Users\\matsj\\Programming\\Personal Project\\pdf_process_site\\pdf_process_site\\extractor\\file.pdf', "wb+") as destination:
+    with open(os.path.join(MEDIA_DIR, 'text.pdf'), "wb+") as destination:
         for chunk in file.chunks():
             destination.write(chunk)
 
