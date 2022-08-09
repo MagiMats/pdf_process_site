e409993a03f07cc1f59ed69b5a8156854dca76b1
MagiMats <matsjanssens2004@gmail.com>
2022-08-09 10:08:39 +0200

    second working test

--- pdf_process_site/extractor/tests.py
+++ pdf_process_site/extractor/tests.py
@@ -7,3 +7,13 @@ class FormTest(TestCase):
         response = self.client.get(reverse('home'))
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'extractor\home.html')
+
+    def test_form(self):
+        response = self.client.post(reverse('home'), {
+            'name': "Form_Test",
+            'email': 'formtest@gmail.com',
+            'upload': 'formtest.pdf',
+            'database_url': 'http://formtest',
+            'link_url': 'http://formtest'
+        })
+        self.assertEqual(response.status_code, 200)
