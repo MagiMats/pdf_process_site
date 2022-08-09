539a0f78fa768812ed30951ab601571ee75a6597
MagiMats <matsjanssens2004@gmail.com>
2022-08-09 09:44:00 +0200

    working tests

--- pdf_process_site/extractor/tests.py
+++ pdf_process_site/extractor/tests.py
@@ -1,3 +1,9 @@
 from django.test import TestCase
+from django.urls import reverse
 
 # Create your tests here.
+class FormTest(TestCase):
+    def test_home_view(self):
+        response = self.client.get(reverse('home'))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'extractor\home.html')
