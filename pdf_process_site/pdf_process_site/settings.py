12eed322c427bc089da5cd3243b58a97ab820eb5
MagiMats <matsjanssens2004@gmail.com>
2022-08-09 09:31:10 +0200

    First css test working

--- pdf_process_site/pdf_process_site/settings.py
+++ pdf_process_site/pdf_process_site/settings.py
@@ -120,6 +120,11 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATIC_DIR = os.path.join(BASE_DIR, 'static')
+
+STATICFILES_DIRS = (
+  os.path.join(BASE_DIR, 'static'),
+)
+
 MEDIA_URL = '/media/'
 MEDIA_DIR = os.path.join(BASE_DIR, 'media')
 # Default primary key field type
